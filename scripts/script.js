5e02a60058b93ba1818380fb7b08e36e03f6651d
leti40 <chrono021@gmail.com>
2022-06-28 01:11:47 +0300

    Завершение работы

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let popupSaveButton = document.querySelector('.popup__save-button');
+let popupTitle = document.querySelector('.popup__element_type_title');
+let popupSubtitle = document.querySelector('.popup__element_type_subtitle');
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileText = document.querySelector('.profile__text');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    popupTitle.value = profileName.textContent;
+    popupSubtitle.value = profileText.textContent;
+}
+profileEditButton.addEventListener('click', openPopup);
+
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+popupCloseButton.addEventListener('click', closePopup);
+
+
+let formElement = document.querySelector('.popup__container');
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    let popupTitle = formElement.querySelector('.popup__element_type_title');
+    let popupSubtitle = formElement.querySelector('.popup__element_type_subtitle');
+    let profileName = document.querySelector('.profile__name');
+    let profileText = document.querySelector('.profile__text');
+
+    profileName.textContent = popupTitle.value;
+    profileText.textContent = popupSubtitle.value;
+    closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
