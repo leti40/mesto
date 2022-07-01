5a49a85a047dcd114fdb3491846451b1a04613e6
leti40 <chrono021@gmail.com>
2022-07-01 17:09:31 +0300

    работа над ошибками

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ let popupSubtitle = document.querySelector('.popup__element_type_subtitle');
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
+let formElement = document.querySelector('.popup__container');
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -21,14 +22,12 @@ function closePopup() {
 popupCloseButton.addEventListener('click', closePopup);
 
 
-let formElement = document.querySelector('.popup__container');
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let popupTitle = formElement.querySelector('.popup__element_type_title');
-    let popupSubtitle = formElement.querySelector('.popup__element_type_subtitle');
-    let profileName = document.querySelector('.profile__name');
-    let profileText = document.querySelector('.profile__text');
+    // let popupTitle = formElement.querySelector('.popup__element_type_title');
+    // let popupSubtitle = formElement.querySelector('.popup__element_type_subtitle');
+    // let profileName = document.querySelector('.profile__name');
+    // let profileText = document.querySelector('.profile__text');
 
     profileName.textContent = popupTitle.value;
     profileText.textContent = popupSubtitle.value;
